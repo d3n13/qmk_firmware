f7466caa8a0f6b6701d98bf0f96755341abe29f9
Denis Bilenko <goldenrodcap@gmail.com>
2025-03-23 14:50:42 +0400

    chore(chary 3x5): rm unused code

--- keyboards/bastardkb/charybdis/3x5/keymaps/default/config.h
+++ keyboards/bastardkb/charybdis/3x5/keymaps/default/config.h
@@ -16,21 +16,7 @@
  */
 #pragma once
 
-#ifdef VIA_ENABLE
-/* VIA configuration. */
-#    define DYNAMIC_KEYMAP_LAYER_COUNT 6
-#endif // VIA_ENABLE
-
 #ifndef __arm__
 /* Disable unused features. */
 #    define NO_ACTION_ONESHOT
 #endif // __arm__
-
-/* Charybdis-specific features. */
-
-#ifdef POINTING_DEVICE_ENABLE
-// Automatically enable the pointer layer when moving the trackball.  See also:
-// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS`
-// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD`
-// #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-#endif // POINTING_DEVICE_ENABLE
--- keyboards/bastardkb/charybdis/3x5/keymaps/default/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/default/keymap.c
@@ -16,10 +16,6 @@
  */
 #include QMK_KEYBOARD_H
 
-#ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-#    include "timer.h"
-#endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-
 enum custom_keycodes {
     JS_ARROW_FN = SAFE_RANGE,
     JS_FN,
@@ -71,25 +67,9 @@ enum charybdis_keymap_layers {
     LAYER_NUMERAL,
     LAYER_GAMING,
     LAYER_GAMING_NUMS,
-    LAYER_POINTER,
     LAYER_MACROS_AND_SETUP,
     LAYER_SETUP
 };
-
-// Automatically enable sniping-mode on the pointer layer.
-#define CHARYBDIS_AUTO_SNIPING_ON_LAYER LAYER_POINTER
-
-#ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-static uint16_t auto_pointer_layer_timer = 0;
-
-#    ifndef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS
-#        define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS 1000
-#    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS
-
-#    ifndef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
-#        define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD 8
-#    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
-#endif     // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
 #define BSP_SYM LT(LAYER_SYMBOLS, KC_BSPC)
 #define TAB_SYM LT(LAYER_SYMBOLS, KC_TAB)
@@ -101,8 +81,6 @@ static uint16_t auto_pointer_layer_timer = 0;
 #    define SNIPING KC_NO
 #endif // !POINTING_DEVICE_ENABLE
 
-// clang-format off
-/** \brief QWERTY layout (3 rows, 10 columns). */
 #define LAYOUT_LAYER_BASE                                                                          \
     KC_Q, KC_W, KC_E, KC_R, KC_T,       KC_Y, KC_U, KC_I,    KC_O,   KC_P,                         \
     KC_A, KC_S, KC_D, KC_F, KC_G,       KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,                      \
@@ -112,52 +90,18 @@ static uint16_t auto_pointer_layer_timer = 0;
 /** Convenience row shorthands. */
 #define ___________TRANSPARENT_HALF_ROW____________ _______, _______, _______, _______, _______
 
-/*
- * Layers used on the Charybdis Nano.
- *
- * These layers started off heavily inspired by the Miryoku layout, but trimmed
- * down and tailored for a stock experience that is meant to be fundation for
- * further personalization.
- *
- * See https://github.com/manna-harbour/miryoku for the original layout.
- */
-
-/**
- * \brief Numeral layout.
- *
- * Primary left-hand layer (right home thumb) is numerals and symbols. Numerals
- * are in the standard numpad locations with symbols in the remaining positions.
- * `KC_DOT` is duplicated from the base layer.
- */
 #define LAYOUT_LAYER_NUMERAL \
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,             KC_7,    KC_8,    KC_9,    KC_0,   \
     DRGSCRL, SNIPING, KC_WH_U, _______, _______,        KC_CAPS,          DRGSCRL, DRGSCRL, DRGSCRL, KC_VOLU, \
     KC_ESC,  KC_WH_L, KC_WH_D, KC_WH_R, KC_CAPS,        TG(LAYER_GAMING), KC_BTN1, KC_BTN2, KC_BTN2, KC_VOLD, \
                       _______, _______, _______,        KC_BTN1, KC_BTN2
 
-/**
- * \brief Symbols layer.
- *
- * Secondary left-hand layer has shifted symbols in the same locations to reduce
- * chording when using mods with shifted symbols. `KC_LPRN` is duplicated next to
- * `KC_RPRN`.
- */
 #define LAYOUT_LAYER_SYMBOLS \
       KC_PLUS, KC_LCBR, KC_RCBR, KC_ASTR, KC_HASH,                     KC_PERC, KC_EQL,  KC_EXLM, KC_UNDS,  KC_KP_MINUS, \
       KC_AT,   KC_LPRN, KC_RPRN, KC_DLR,  KC_TILDE,                    KC_GRV,  KC_QUES, KC_UP,   KC_QUOT,  KC_PIPE, \
       KC_GRV,  KC_LBRC, KC_RBRC, KC_CIRC, KC_COLN,                     KC_AMPR, KC_LEFT, KC_DOWN, KC_RIGHT, KC_BACKSLASH, \
                                  _______, _______, _______,   _______, _______
 
-/**
- * \brief Add Home Row mod to a layout.
- *
- * Expects a 10-key per row layout.  Adds support for GACS (Gui, Alt, Ctl, Shift)
- * home row.  The layout passed in parameter must contain at least 20 keycodes.
- *
- * This is meant to be used with `LAYER_ALPHAS_QWERTY` defined above, eg.:
- *
- *     HOME_ROW_MOD_GACS(LAYER_ALPHAS_QWERTY)
- */
 #define _HOME_ROW_MOD(                                            \
     L00, L01, L02, L03, L04,   R05, R06, R07, R08, R09, \
     L10, L11, L12, L13, L14,   R15, R16, R17, R18, R19,                  \
@@ -169,22 +113,12 @@ static uint16_t auto_pointer_layer_timer = 0;
       __VA_ARGS__
 #define HOME_ROW_MOD(...) _HOME_ROW_MOD(__VA_ARGS__)
 
-/** \brief Mouse emulation and pointer functions. */
-#define LAYOUT_LAYER_POINTER                                                                  \
-    QK_BOOT,  EE_CLR, XXXXXXX, DPI_MOD, S_D_MOD, S_D_MOD, DPI_MOD, XXXXXXX,  EE_CLR, QK_BOOT, \
-    ___________TRANSPARENT_HALF_ROW____________, ___________TRANSPARENT_HALF_ROW____________, \
-    _______, DRGSCRL, SNIPING, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, SNIPING, DRGSCRL, _______, \
-                      KC_BTN2, KC_BTN1, KC_BTN3, KC_BTN3, KC_BTN1
-
-
-
 #define LAYOUT_wrapper(...) LAYOUT(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_wrapper(HOME_ROW_MOD(LAYOUT_LAYER_BASE)),
   [LAYER_NUMERAL] = LAYOUT_wrapper(LAYOUT_LAYER_NUMERAL),
   [LAYER_SYMBOLS] = LAYOUT_wrapper(LAYOUT_LAYER_SYMBOLS),
-  [LAYER_POINTER] = LAYOUT_wrapper(LAYOUT_LAYER_POINTER),
   [LAYER_GAMING] = LAYOUT_wrapper(
       KC_ESC,  KC_Q, KC_W, KC_E,  KC_R,            KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,
       KC_LSFT, KC_A, KC_S, KC_D,  KC_F,            KC_H,   KC_J,    KC_K,    KC_L,    KC_WH_U,
@@ -210,48 +144,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  _______, _______, _______,    _______, _______
    ),
 };
-// clang-format on
-
-#ifdef POINTING_DEVICE_ENABLE
-#    ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
-        if (auto_pointer_layer_timer == 0) {
-            layer_on(LAYER_POINTER);
-#        ifdef RGB_MATRIX_ENABLE
-            rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
-            rgb_matrix_sethsv_noeeprom(HSV_GREEN);
-#        endif // RGB_MATRIX_ENABLE
-        }
-        auto_pointer_layer_timer = timer_read();
-    }
-    return mouse_report;
-}
-
-void matrix_scan_user(void) {
-    if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
-        auto_pointer_layer_timer = 0;
-        layer_off(LAYER_POINTER);
-#        ifdef RGB_MATRIX_ENABLE
-        rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);
-#        endif // RGB_MATRIX_ENABLE
-    }
-}
-#    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-
-#    ifdef CHARYBDIS_AUTO_SNIPING_ON_LAYER
-layer_state_t layer_state_set_user(layer_state_t state) {
-    charybdis_set_pointer_sniping_enabled(layer_state_cmp(state, CHARYBDIS_AUTO_SNIPING_ON_LAYER));
-    return state;
-}
-#    endif // CHARYBDIS_AUTO_SNIPING_ON_LAYER
-#endif     // POINTING_DEVICE_ENABLE
-
-#ifdef RGB_MATRIX_ENABLE
-// Forward-declare this helper function since it is defined in
-// rgb_matrix.c.
-void rgb_matrix_update_pwm_buffers(void);
-#endif
 
 enum combos {
   ZX_SETUP_LAYER
